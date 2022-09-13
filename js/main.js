446af6e1920731b9f17b8e6cb972059bd92c6493
adriannamunoz <105665083+adriannamunoz@users.noreply.github.com>
2022-09-13 12:08:29 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("testing");
+const listsContainer = document.querySelector('data-lists')
+
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'todo'
+}]
+
+function render() {
+    <li class="list-name">Work</li>
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add("list-name")
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
